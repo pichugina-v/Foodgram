f6624271bdb0012265290327f76b989b657ec19a
pichugina-v <Psyde26@yandex.ru>
2021-09-28 20:53:43 +0300

    user model

---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты',
+        unique=True
+    )
+    username = models.CharField(
+        verbose_name='Ник',
+        max_length=128,
+        unique=True
+    )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=30,
+        blank=True,
+        null=True
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=30,
+        blank=True,
+        null=True
+    )
